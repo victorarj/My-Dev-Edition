9ced60385f3f36747b0f2725855702967eb2a2a0
Victor Araujo <victor.araujo@thomsonreuters.com>
2022-10-27 09:20:34 +0100

    creating and dispatching of events

---
+++ force-app/main/default/lwc/childComponent2/childComponent2.js
@@ -0,0 +1,10 @@
+import { LightningElement } from 'lwc';
+
+export default class ChildComponent2 extends LightningElement {
+    handleChange(event)
+    {
+        const name=event.target.value;
+        const selectEvent=new CustomEvent('mycustomevent',{detail:name,bubbles:true});
+        this.dispatchEvent(selectEvent);
+    }
+}
