f287ee96920718ec1d152673869fe929d0355262
Victor Araujo <victor.araujo@thomsonreuters.com>
2024-03-09 18:13:23 +0000

    Using public getter and setter

--- force-app/main/default/lwc/numerator/numerator.js
+++ force-app/main/default/lwc/numerator/numerator.js
@@ -1,7 +1,17 @@
 import { LightningElement, api } from 'lwc';
 
 export default class Numerator extends LightningElement {
-    @api counter = 0;
+    //@api counter = 0;
+    _currentCount = 0;
+    priorCount = 0;
+    @api
+    get counter() {
+      return this._currentCount;
+    }
+    set counter(value) {
+      this.priorCount = this._currentCount;
+      this._currentCount = value;
+    }
     handleIncrement() {
       this.counter++;
     }
